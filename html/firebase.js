f568976cb2ddf987ac684a80bfe38551734d08e4
Ishaan Potle <ipotle007@gmail.com>
2021-10-28 22:18:34 +0530

    commit

---
+++ html/firebase.js
@@ -0,0 +1,15 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyBuZAIs9Ru18YrJQPWMu6nm0lg5QO0e5CI",
+    authDomain: "astronetic-3afaa.firebaseapp.com",
+    projectId: "astronetic-3afaa",
+    storageBucket: "astronetic-3afaa.appspot.com",
+    messagingSenderId: "746661183819",
+    appId: "1:746661183819:web:92436d31332da2bbbfe8ce",
+    measurementId: "G-N5Z5XZMS69"
+  };
+
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
+  firebase.getAnalytics(app);
+
+  
